5fc2eae903a3b2a1f3ddb196e48209aa623e559c
elhanbthomas <elhanbthomas@gmail.com>
2025-09-28 23:13:20 +0530

    graph

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include "../include/QuantumCircuit.h" 
+#include "../include/matplotlibcpp.h"
 #include <iostream>
 #include <ctype.h>
 using namespace std;
 
+namespace plt = matplotlibcpp;
 // int main() {
 //     cout << "Creating a 2-qubit circuit." << endl;
 //     QuantumCircuit qc(2);
@@ -38,6 +40,44 @@ void displayCircuit(vector<vector<char>>& circuit) {
         }
     }
     cout << endl;
+}
+
+void displayPlot(){
+        // 1. DATA GENERATION
+    // Your C++ program generates the data for the qubit states.
+    std::vector<std::string> states = {"|00>", "|01>", "|10>", "|11>"};
+    std::vector<double> percentages = {85.5, 4.2, 5.3, 5.0};
+
+    // 2. CREATE THE PLOT
+    // Set the figure size for better readability
+    plt::figure_size(800, 600);
+
+    // Create the bar plot using the percentage values
+    plt::bar(percentages);
+
+    // 3. CUSTOMIZE THE PLOT
+    plt::title("Qubit Measurement Probabilities");
+    plt::xlabel("Qubit State");
+    plt::ylabel("Percentage (%)");
+    
+    // Set the y-axis to range from 0 to 100
+    plt::ylim(0, 100);
+
+    // Set the x-axis ticks to correspond to our state labels.
+    // This is a key step for categorical data.
+    // We create a vector of positions [0, 1, 2, 3] for the ticks.
+    std::vector<double> ticks(states.size());
+    for(size_t i = 0; i < states.size(); ++i) {
+        ticks[i] = i;
+    }
+    plt::xticks(ticks, states);
+
+    // 4. SAVE OR SHOW THE PLOT
+    // std::cout << "Saving plot to qubit_plot_mpl.png..." << std::endl;
+    // plt::save("qubit_plot_mpl.png");
+
+    // Alternatively, to display the plot in a GUI window:
+    plt::show();
 }
 
 void applyGate(QuantumCircuit& qc, int n, vector<vector<char>>& circuit) {
@@ -89,7 +129,7 @@ int main() {
         
         switch(ch) {
             case 1: applyGate(qc, n, circuit); break;
-            case 2: qc.measure(); choice = 'n'; break;
+            case 2: qc.measure(); choice = 'n'; displayPlot(); break;
             case 3: displayCircuit(circuit);
         }
     }
