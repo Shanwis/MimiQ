32ff9bbe2f95c71af53fee2fe76e641bfbd8a4be
elhanbthomas <elhanbthomas@gmail.com>
2025-09-28 14:02:39 +0530

    new commit

--- simulator.cpp
+++ simulator.cpp
@@ -16,6 +16,11 @@ private:
         {1.0 / sqrt(2), -1.0 / sqrt(2)}
     };
 
+    vector<vector<double>> Z_MATRIX = {
+        {1.0,0.0},
+        {0.0,-1.0}
+    };
+
     vector<vector<double>> matrixMultiply(const vector<vector<double>>& A,
                                           const vector<vector<double>>& B) {
         int m = A.size();        // rows in A
@@ -39,36 +44,60 @@ private:
         return result;
     }
 
+    vector<vector<double>> tensorProduct(const vector<vector<double>>& A,
+                                     const vector<vector<double>>& B) {
+    int m = A.size();
+    int n = B.size();
+
+    vector<vector<double>> result(m * n, vector<double>(1, 0.0));
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            result[i * n + j][0] = A[i][0] * B[j][0];
+        }
+    }
+    return result;
+    }
+
 public:
-    QuantumCircuit() {
+    QuantumCircuit(int n) {
         vector<vector<double>> qubit(2, vector<double>(1, 0.0));
         qubit[0][0] = 1.0; // |0> state
-        mp[0] = qubit;
+        for(int i=0;i<n;i++) {
+           mp[i] = qubit;
+        }
     }
 
-    void X() {
-        mp[0] = matrixMultiply(NOT_MATRIX, mp[0]);
+    void X(int n) {
+        mp[n] = matrixMultiply(NOT_MATRIX, mp[n]);
     }
 
-    void H() {
-        mp[0] = matrixMultiply(HADAMARD_MATRIX, mp[0]);
+    void H(int n) {
+        mp[n] = matrixMultiply(HADAMARD_MATRIX, mp[n]);
+    }
+
+    void Z(int n) {
+        mp[n] = matrixMultiply(Z_MATRIX, mp[n]);
     }
 
     void measure() {
-    double p0 = mp[0][0][0] * mp[0][0][0]; // |alpha|^2
-    double p1 = mp[0][1][0] * mp[0][1][0]; // |beta|^2
+        vector<vector<double>> combined = tensorProduct(mp[0], mp[1]);
 
-    cout << fixed << setprecision(6);
-    cout << "Probability of |0>: " << p0 << endl;
-    cout << "Probability of |1>: " << p1 << endl;
-}
+        vector<string> basis = {"00", "01", "10", "11"};
+
+        cout << fixed << setprecision(6);
+        for (int i = 0; i < combined.size(); i++) {
+            double prob = combined[i][0] * combined[i][0]; // |amplitude|^2
+            cout << "Probability of |" << basis[i] << ">: " << prob << endl;
+        }
+    }
 };
 
 int main() {
-    QuantumCircuit qc;
-    // qc.X();
-    qc.H();
-    qc.H();   // Apply Hadamard
+    QuantumCircuit qc(2);
+    qc.X(1);
+    qc.H(0);
     qc.measure();
     return 0;
 }
+
